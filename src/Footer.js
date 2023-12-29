25623774de28599eb72bbac6f116ae74a76a2a10
evansleong <141153962+evansleong@users.noreply.github.com>
2023-12-29 15:00:02 +0800

    ggg

--- src/Footer.js
+++ src/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
         <div className=' flex flex-row mx-auto w-5/6 items-center justify-center gap-x-2'>
             <a
                 className="flex justify-center hover:text-brightColor transition-all cursor-pointer"
-                href="https://instagram.com/xinru__0202?igshid=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr"
+                href="https://www.instagram.com/gao_chong_/"
             >
                 <BsInstagram size={25} />
             </a>
-            <p>@xinru__0202</p>
+            <p>@gao_chong_</p>
         </div>
 
     )
